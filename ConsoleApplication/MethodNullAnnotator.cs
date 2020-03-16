1f81a8406967cebbd2358c33893e78f57c13cc61
Daniel Kudernatsch <daniel.kudernatsch@rubicon.eu>
2020-03-16 10:28:44 +0100

    simple null checks

---
+++ ConsoleApplication/MethodNullAnnotator.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace NullableTypeSetter.ConsoleApplication
+{
+ public class MethodNullAnnotator : CSharpSyntaxRewriter
+  {
+    private SemanticModel _semanticModel;
+
+    public MethodNullAnnotator (SemanticModel semanticModel)
+    {
+      _semanticModel = semanticModel;
+    }
+
+    public override SyntaxNode? VisitMethodDeclaration (MethodDeclarationSyntax node)
+    {
+      if (DoesReturnVoid (node))
+        return node;
+      if (node.Body == null)
+        return node;
+      if (node.Body?.Statements.Count == 0)
+        return node;
+
+      var retrunsNull = HasCanBeNullAttribute (node)
+                        || ReturnsNull (node, _semanticModel);
+
+      if (!retrunsNull)
+        return node;
+
+      return node.WithReturnType (
+          MakeNullable (node.ReturnType));
+    }
+
+    private static bool ReturnsNull (MethodDeclarationSyntax node, SemanticModel semanticModel)
+    {
+      var returnStatements = semanticModel.AnalyzeControlFlow (
+              node.Body?.Statements.First(),
+              node.Body?.Statements.Last())
+          .ReturnStatements;
+
+      return returnStatements.Any (
+          stmt => stmt is ReturnStatementSyntax returnStatement
+                  && IsDefinitlyNull (returnStatement.Expression!, semanticModel));
+    }
+
+    private static bool HasCanBeNullAttribute (MethodDeclarationSyntax node)
+    {
+      return node.AttributeLists.SelectMany (list => list.Attributes)
+          .Any (attr => attr.Name.ToString().EndsWith ("CanBeNull"));
+    }
+
+    private static bool DoesReturnVoid (MethodDeclarationSyntax node)
+    {
+      return node.ReturnType is PredefinedTypeSyntax type
+             && type.Keyword.Kind() == SyntaxKind.VoidKeyword;
+    }
+
+    public static bool IsDefinitlyNull (ExpressionSyntax expression, SemanticModel semanticModel)
+    {
+      var typeInfo = semanticModel.GetTypeInfo (expression);
+
+      if (typeInfo.Nullability.FlowState != NullableFlowState.None
+          || typeInfo.Nullability.Annotation == NullableAnnotation.Annotated)
+      {
+        Console.WriteLine (expression);
+      }
+
+      return expression switch
+      {
+          LiteralExpressionSyntax literal => literal.Kind() == SyntaxKind.NullLiteralExpression ||
+                                             literal.Kind() == SyntaxKind.DefaultExpression,
+          InvocationExpressionSyntax invocation => InvokesNullReturningMethod (invocation, semanticModel),
+          _ => false,
+      };
+    }
+
+    private static bool InvokesNullReturningMethod (InvocationExpressionSyntax invocation, SemanticModel semanticModel)
+    {
+      var method = semanticModel.GetSymbolInfo (invocation)
+          .Symbol as IMethodSymbol;
+      if (method == null)
+        return false;
+      if (!method.ReturnType.IsReferenceType)
+        return false;
+
+      return method.DeclaringSyntaxReferences.Any (syntaxRef => DoesReturnVoid ((MethodDeclarationSyntax) syntaxRef.GetSyntax()));
+    }
+
+    public static TypeSyntax MakeNullable (TypeSyntax typeSyntax)
+    {
+      if (typeSyntax is NullableTypeSyntax)
+        return typeSyntax;
+      var nullable = SyntaxFactory.NullableType (typeSyntax.WithoutTrailingTrivia());
+      return nullable
+          .WithTrailingTrivia (typeSyntax.GetTrailingTrivia());
+    }
+  }
+}
--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,11 +1,89 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.MSBuild;
 
 namespace NullableTypeSetter.ConsoleApplication
 {
   internal class Program
   {
-    public static void Main (string[] args)
+    public static async Task Main (string[] args)
     {
+      if (args.Length != 2)
+      {
+        Console.WriteLine ("Please supply a solution directory and a project name to convert.");
+        Console.WriteLine ("eg.:     nrtRewriter \"C:\\Develop\\MyCode\\MyCode.sln\" Core.Utils");
+      }
+
+      var solutionPath = args[0];
+      var projectName = args[1];
+
+      var solution = await LoadSolutionSpace (solutionPath);
+      var project = LoadProject (solution, projectName);
+
+      foreach (var document in project.Documents)
+      {
+        var newDocument = await Convert (document);
+        await WriteChanges(document, newDocument);
+      }
+    }
+
+    private static async Task WriteChanges (Document oldDocument, Document newDocument)
+    {
+      try
+      {
+        var newRootNode = await newDocument.GetSyntaxRootAsync();
+        var oldRootNode = await oldDocument.GetSyntaxRootAsync();
+
+        if (oldRootNode == newRootNode) return;
+
+        using var fileStream = new FileStream (newDocument.FilePath!, FileMode.Truncate);
+        using var writer = new StreamWriter (fileStream, Encoding.Default);
+        newRootNode!.WriteTo (writer);
+      }
+      catch (IOException ex)
+      {
+        throw new InvalidOperationException ($"Unable to write source file '{newDocument.FilePath}'.", ex);
+      }
+    }
+
+    private static async Task<Document> Convert (Document document)
+    {
+      var semanticModel = await document.GetSemanticModelAsync()
+                          ?? throw new Exception();
+      var syntaxRoot = await document.GetSyntaxRootAsync()
+                       ?? throw new Exception();
+
+      var rewritten = new MethodNullAnnotator(semanticModel).Visit(syntaxRoot);
+
+      return document.WithSyntaxRoot (rewritten);
+    }
+
+    private static Project LoadProject (Solution solution, string projectName)
+    {
+      var project = solution.Projects.Single(p => p.Name == projectName);
+      var compilationOptions = project.CompilationOptions as CSharpCompilationOptions;
+
+      compilationOptions = compilationOptions?.WithNullableContextOptions (NullableContextOptions.Enable);
+      if (compilationOptions != null)
+        project = project.WithCompilationOptions (compilationOptions);
+
+      return project;
+    }
+
+    public static Task<Solution> LoadSolutionSpace (string solutionPath)
+    {
+      var msBuild = MSBuildLocator.QueryVisualStudioInstances().First();
+      MSBuildLocator.RegisterInstance (msBuild);
+      using var workspace = MSBuildWorkspace.Create();
+      workspace.WorkspaceFailed += (sender, args) => { Console.WriteLine (args.Diagnostic); };
+
+      return workspace.OpenSolutionAsync (@"C:\Development\Remotion-git\Remotion.sln");
     }
   }
 }
